337fdd99b1733c4620b0f8aae08b91a9157e59b5
Maxim <MaximKomoza@yandex.ru>
2023-11-07 16:44:47 +0700

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Header } from './components/header/header';
 import { Main } from './pages/main/main';
 
 function App() {
     return (
         <div className="container">
+            <Header />
             <div className="wrapper">
                 <Main />
             </div>
---
+++ src/components/header/header.tsx
@@ -0,0 +1,26 @@
+import './header.scss';
+
+const isLogin: boolean = false;
+
+export const Header = () => {
+    return (
+        <div className="header">
+            {isLogin ? (
+                <div className="header__user-actions user-actions">
+                    <button className="user-actions__place-an-ad header__button">
+                        Разместить объявление
+                    </button>
+                    <button className="user-actions__profile header__button">
+                        Личный кабинет
+                    </button>
+                </div>
+            ) : (
+                <div className="header__login login">
+                    <button className="login__button header__button">
+                        Вход в личный кабинет
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
